85889676c736b77011f9dfc1a63a1f2bce488e4d
William Sheu <sheu.william@gmail.com>
2014-10-30 15:04:57 -0700

    Fixed character widths to be slightly less gimmicky.

--- penguingui/Label.lua
+++ penguingui/Label.lua
@@ -30,7 +30,7 @@ end
 
 -- Recalculates the bounds of the label based on its text and font size.
 function Label:recalculateBounds()
-  self.width = self.getStringWidth(self.text, self.fontSize)
+  self.width = PtUtil.getStringWidth(self.text, self.fontSize)
   self.height = self.fontSize
 end
 
@@ -50,18 +50,4 @@ function Label:draw()
                                     }, self.fontSize, self.fontColor)
 
   Component.draw(self)
-end
-
--- Get the approximate pixel width of a string.
---
--- @param text The string to get the width of.
--- @param fontSize The size of the font to get the width from.
-function Label.getStringWidth(text, fontSize)
-  local widths = PtUtil.getCharWidth
-  local scale = PtUtil.getFontScale(fontSize)
-  local out = 0
-  for i=1,#text,1 do
-    out = out + widths(string.byte(text, i))
-  end
-  return out * scale
 end
