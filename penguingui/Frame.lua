2db2ceb2ff9d8b4cc361493837aa7361b0b17712
PenguinToast <william@PenguinToast>
2014-10-31 14:21:50 -0700

    Minor changes, nothing important.

--- penguingui/Frame.lua
+++ penguingui/Frame.lua
@@ -1,10 +1,12 @@
 -- A window
 Frame = class(Panel)
+Frame.borderColor = "black"
+Frame.backgroundColor = "#545454"
 
 function Frame:_init()
   Component._init(self)
 end
 
 function Frame:draw(dt)
-
+  
 end
