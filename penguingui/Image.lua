97f5373b79d91014f6b329b7b4e789e5a60c7b81
William Sheu <sheu.william@gmail.com>
2014-11-08 12:53:14 -0800

    Changed Image component to automatically size the image

--- penguingui/Image.lua
+++ penguingui/Image.lua
@@ -5,17 +5,17 @@ Image = class(Component)
 --
 -- @param x The x coordinate of the new component, relative to its parent.
 -- @param y The y coordinate of the new component, relative to its parent.
--- @param width The width of the new component.
--- @param height The height of the new component.
 -- @param image The path to the image.
 -- @param scale (Optional) Factor to scale the image by.
-function Image:_init(x, y, width, height, image, scale)
+function Image:_init(x, y, image, scale)
   Component._init(self)
-
+  scale = scale or 1
+  
   self.x = x
   self.y = y
-  self.width = width
-  self.height = height
+  local imageSize = root.imageSize(image)
+  self.width = imageSize[1] * scale
+  self.height = imageSize[2] * scale
   self.image = image
   self.scale = scale
 end
@@ -24,7 +24,7 @@ function Image:draw(dt)
   local startX = self.x + self.offset[1]
   local startY = self.y + self.offset[2]
   local image = self.image
-  local scale = self.scale or 1
+  local scale = self.scale
   
   PtUtil.drawImage(image, {startX, startY}, scale)
 end
